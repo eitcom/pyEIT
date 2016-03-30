2abd8c935d5448e6f96ae2a76b120655c529f357
by liu <liubenyuan@gmail.com>
2016-03-30 09:58:10 +0800

    add experimental distmesh3d demo

--- demo/demo_dynamic_bp.py
+++ demo/demo_dynamic_bp.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
 import pyeit.mesh as mesh
 from pyeit.eit.fem import forward, pdeprtni
@@ -10,7 +9,7 @@ from pyeit.eit.utils import eit_scan_lines
 import pyeit.eit.bp as bp
 
 """ 0. build mesh """
-ms, elPos = mesh.create(16, h0=0.12)
+ms, elPos = mesh.create(16, h0=0.075)
 
 # extract node, element, alpha
 no2xy = ms['node']
@@ -25,14 +24,16 @@ ms0 = mesh.set_alpha(ms, anom=anomaly, background=1.0)
 anomaly = [{'x': 0.5, 'y': 0.5, 'd': 0.1, 'alpha': 100.0}]
 ms1 = mesh.set_alpha(ms, anom=anomaly, background=1.0)
 
-# draw delta alpha
+# draw
 delta_alpha = np.real(ms1['alpha'] - ms0['alpha'])
 fig, ax = plt.subplots()
 im = ax.tripcolor(no2xy[:, 0], no2xy[:, 1], el2no, delta_alpha,
-                  shading='flat')
+                  shading='flat', cmap=plt.cm.viridis)
 ax.set_title(r'$\Delta$ Conductivities')
 fig.colorbar(im)
-fig.set_size_inches(6, 4.5)
+ax.axis('equal')
+fig.set_size_inches(6, 3)
+# fig.savefig('demo_bp.png', dpi=96)
 plt.show()
 
 """ 2. FEM forward simulations """
@@ -45,35 +46,6 @@ fwd = forward(ms, elPos)
 f0 = fwd.solve(exMtx, step=step, perm=ms0['alpha'])
 f1 = fwd.solve(exMtx, step=step, perm=ms1['alpha'])
 
-""" 2.1 show equi-potential lines """
-
-# in python, index start from 0
-exLine = exMtx[1].ravel()
-
-# change alpha
-anomaly = [{'x': 0.5, 'y': 0.5, 'd': 0.1, 'alpha': 100.0}]
-ms_test = mesh.set_alpha(ms, anom=anomaly, background=1.0)
-tri_perm = ms_test['alpha']
-
-# solving once using fem
-f, _ = fwd.solve_once(exLine, tri_perm)
-
-# draw
-fig = plt.figure()
-gs = gridspec.GridSpec(1, 2)
-# subplot
-ax1 = fig.add_subplot(gs[0, 0])
-ax1.tricontour(no2xy[:, 0], no2xy[:, 1], el2no, np.real(f),
-               40, linewidth=0.5,
-               cmap=plt.cm.viridis)
-ax1.tripcolor(no2xy[:, 0], no2xy[:, 1], el2no, np.real(tri_perm),
-              edgecolors='k', shading='flat', alpha=0.5,
-              cmap=plt.cm.Greys)
-ax1.plot(no2xy[elPos, 0], no2xy[elPos, 1], 'ro')
-ax1.set_title('equi-potential lines')
-ax1.axis('equal')
-ax1.axis([-1, 1, -1, 1])
-
 """
 3. naive inverse solver using back-projection
 """
@@ -82,10 +54,11 @@ ds = eit.solve(f1.v, f0.v, normalize=True)
 ds = 10000. * pdeprtni(no2xy, el2no, ds)
 
 # draw
-ax2 = fig.add_subplot(gs[0, 1])
-im = ax2.tripcolor(no2xy[:, 0], no2xy[:, 1], el2no, ds)
-ax2.set_title(r'$\Delta$ Conductivities')
-ax2.axis('equal')
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+im = ax1.tripcolor(no2xy[:, 0], no2xy[:, 1], el2no, ds, cmap=plt.cm.viridis)
+ax1.set_title(r'$\Delta$ Conductivities')
+ax1.axis('equal')
 fig.colorbar(im)
 """ for production figures, use dpi=300 or render pdf """
 fig.set_size_inches(6, 3)
