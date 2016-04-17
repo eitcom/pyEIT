1ac9ff2feed2945debf0ecf81c62676488c76d95
liubenyuan <liubenyuan@gmail.com>
2016-04-17 08:52:21 +0800

    vispy may not available in standard distributions

--- pyeit/mesh/plot/__init__.py
+++ pyeit/mesh/plot/__init__.py
@@ -1,4 +1,9 @@
 """ add customized plot for 2D/3D mesh """
 from .voronoi_plot import voronoi_plot
-from .tetplot import tetplot
+
+try:
+    from .tetplot import tetplot
+except ImportError:
+    print("mesh.plot: failed to import tetplot or vispy.")
+
 __all__ = ['voronoi_plot', 'tetplot']
