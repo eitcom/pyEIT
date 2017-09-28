8c44cf1ecab82eb9891ed917689f653076550027
by liu <liubenyuan@gmail.com>
2017-09-28 15:45:27 +0800

    add ati (averaged total impedance) feature

---
+++ pyeit/feature_extraction/static_r.py
@@ -0,0 +1,24 @@
+# static features of R values measured using EIT system
+
+import numpy as np
+
+
+def ati(x):
+    """
+    averaged total impedance, unit (mV),
+    original / 192.0
+    new / 16.0
+    """
+    v = np.abs(x).sum() / 192.0
+    return v
+
+def rchannel(x, offset=0):
+    """
+    calculate R (voltages) of two electrodes counted from the
+    excitation electrode with a fixed number (offset)
+    """
+    N = 12  # measurements within a excitation pattern
+    c = np.mod(offset, N)
+    x = x[:, c:N:]
+    return np.abs(x).sum(axis=1) / float(N)
+
