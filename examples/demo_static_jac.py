e2e50c66d541351dce9e4aef6e2b521167882043
by liu <liubenyuan@gmail.com>
2017-11-10 08:28:48 +0800

    update lamb decay, static_jac now support more background options

--- examples/demo_static_jac.py
+++ examples/demo_static_jac.py
@@ -20,8 +20,8 @@ mesh_obj, el_pos = create(n_el, h0=0.1)
 # test function for altering the permittivity in mesh
 anomaly = [{'x': 0.4, 'y': 0.4, 'd': 0.2, 'perm': 10},
            {'x': -0.4, 'y': -0.4, 'd': 0.2, 'perm': 0.1}]
-# TODO: even if background changed to values other than 1.0 will fail
-mesh_new = set_perm(mesh_obj, anomaly=anomaly, background=1.)
+# background changed to values other than 1.0 requires more iterations
+mesh_new = set_perm(mesh_obj, anomaly=anomaly, background=2.)
 
 # extract node, element, perm
 pts = mesh_obj['node']
@@ -43,11 +43,12 @@ ex_mat = eit_scan_lines(n_el, el_dist)
 fwd = Forward(mesh_obj, el_pos)
 f1 = fwd.solve_eit(ex_mat, step, perm=mesh_new['perm'], parser='std')
 
-""" 3. solve_eit using gaussian-newton """
+""" 3. solve_eit using gaussian-newton (with regularization) """
 # number of stimulation lines/patterns
 eit = jac.JAC(mesh_obj, el_pos, ex_mat, step, perm=1.0, parser='std')
 eit.setup(p=0.25, lamb=1.0, method='lm')
-ds = eit.gn(f1.v, lamb_decay=0.2, lamb_min=1e-4, maxiter=20, verbose=True)
+# lamb = lamb * lamb_decay
+ds = eit.gn(f1.v, lamb_decay=0.1, lamb_min=1e-5, maxiter=20, verbose=True)
 
 # plot
 fig, ax = plt.subplots(figsize=(6, 4))
