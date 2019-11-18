b050de22b7fbdc294542f60bc9472d0b2935bbb7
byliu <liubenyuan@gmail.com>
2019-11-18 08:58:33 +0800

    rename example files

--- examples/mesh_distmesh3d.py
+++ examples/mesh_distmesh3d.py
@@ -4,26 +4,23 @@
 # Copyright (c) Benyuan Liu. All Rights Reserved.
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 from __future__ import division, absolute_import, print_function
-
 import numpy as np
 
 import pyeit.mesh as mesh
 import pyeit.mesh.plot as mplot
 
-# build tetrahedron
-# 3D tetrahedron must have a bbox
-bbox = [[-1, -1, -1], [1, 1, 1]]
-
-# save calling convention as distmesh 2D
-ms, el_pos = mesh.create(h0=0.2, bbox=bbox)
-p = ms['node']
-t = ms['element']
+# tetrahedron meshing in a 3D bbox
+bbox = [[-1.1, -1.1, -1.1], [1.1, 1.1, 1.1]]
+ms, el_pos = mesh.create(h0=0.15, bbox=bbox)
 
 # print mesh quality
+p = ms['node']
+t = ms['element']
 print('points =', p.shape)
 print('simplices =', t.shape)
 
+# plot
+mplot.tetplot(p, t, edge_color=(0.2, 0.2, 1.0, 1.0), alpha=0.01)
 # create random color
 f = np.random.randn(p.shape[0])
-# mplot.tetplot(p, t, edge_color=(0.2, 0.2, 1.0, 1.0), alpha=0.01)
-mplot.tetplot(p, t, f, alpha=0.25)
+# mplot.tetplot(p, t, f, alpha=0.25)
