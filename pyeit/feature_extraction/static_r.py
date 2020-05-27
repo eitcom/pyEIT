8ef4c8f16d6e9d9d0d3a34833dc76b665ab6632c
byliu <liubenyuan@gmail.com>
2020-05-27 19:32:45 +0800

    add ATI lr

--- pyeit/feature_extraction/static_r.py
+++ pyeit/feature_extraction/static_r.py
@@ -20,6 +20,49 @@ def ati(x):
     return v
 
 
+def fmmu_index(n_el=16, dist=8, step=1):
+    """
+    generate FMMU patterns and left, right index
+
+    n_el = 16
+    dist = 8 # opposition
+    step = 1 # adjacent measures
+    """
+    # [0, ..., 15]
+    left_el = [13, 14, 15, 0, 1, 2, 3, 4]
+    right_el = [5, 6, 7, 8, 9, 10, 11, 12]
+
+
+    m_array = []
+    for a in range(n_el):
+        b = (a + dist) % n_el
+        for j in range(a, a+n_el):
+            m = j % n_el
+            n = (j+step) % n_el
+            if not (m in [a, b] or n in [a, b]):
+                diff_pair = [n, m] # v_n - v_m
+                m_array.append(diff_pair)
+
+    m_array = np.array(m_array)
+    N = m_array.shape[0]
+    left_sel = np.zeros(N, dtype=np.bool) # 192
+    right_sel = np.zeros(N, dtype=np.bool)
+    for i, nm in enumerate(m_array):
+        n = nm[0]
+        if n in left_el:
+            left_sel[i] = True
+        if n in right_el:
+            right_sel[i] = True
+
+    return left_sel, right_sel
+
+
+def ati_lr(x, sel):
+    x_sel = np.mean(np.abs(x[sel]))
+
+    return x_sel
+
+
 def ati_df(x):
     """ati of DataFrame"""
     return x.abs().sum(skipna=False) / 192.0
