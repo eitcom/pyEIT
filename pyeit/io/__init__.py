1f6a551587ded5579de3d46d2eacced91ad9c046
Liu Benyuan <liubenyuan@gmail.com>
2020-11-15 19:46:16 +0800

    support et3, ewd, erd file formats.

--- pyeit/io/__init__.py
+++ pyeit/io/__init__.py
@@ -3,6 +3,7 @@
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 from .et3 import ET3
 from .et4 import ET4
+from .ewd import EWD
 from .daeger_eit import DAEGER_EIT
 from .utils import get_date_from_folder
 
@@ -10,4 +11,12 @@ from .utils import get_date_from_folder
 from .mes import load as mes_load
 from .icp import load as icp_load
 
-__all__ = ["ET3", "get_date_from_folder", "ET4", "DAEGER_EIT", "mes_load", "icp_load"]
+__all__ = [
+    "ET3",
+    "ET4",
+    "EWD",
+    "DAEGER_EIT",
+    "get_date_from_folder",
+    "mes_load",
+    "icp_load",
+]
