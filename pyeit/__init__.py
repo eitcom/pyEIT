ae3caef8416d5ae7f9a5c9b791a5944a75d4e482
byliu <liubenyuan@gmail.com>
2022-02-13 19:34:51 +0800

    bump 1.1.4

--- pyeit/__init__.py
+++ pyeit/__init__.py
@@ -6,3 +6,9 @@ eit:
 mesh:
     create triangle(2D) / tetrahedron(3D) meshes
 """
+__author__ = "Benyuan Liu"
+__author_email__ = "liubenyuan@gmail.com"
+__license__ = "BSD"
+APP_NAME = "pyeit"
+APP_VERSION = "1.1.4"
+APP_DESCRIPTION = "A Python Package for EIT"
--- setup.py
+++ setup.py
@@ -1,33 +1,26 @@
+# copied from https://github.com/adrienverge/yamllint/
 import pathlib
-from setuptools import setup, find_packages
+from setuptools import setup
+
+from pyeit import (
+    __author__,
+    __author_email__,
+    __license__,
+    APP_NAME,
+    APP_VERSION,
+    APP_DESCRIPTION,
+)
 
 
-VERSION = "1.1.2"
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
-
 setup(
-    name="pyeit",
-    version=VERSION,
-    description="A Python based framework for EIT (Electrical Impedance Tomography)",
+    name=APP_NAME,
+    version=APP_VERSION,
+    author=__author__,
+    author_email=__author_email__,
+    description=APP_DESCRIPTION.split("\n")[0],
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/liubenyuan/pyEIT",
-    author="Benyuan Liu",
-    author_email="liubenyuan@gmail.com",
-    license="BSD",
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords=["Electrical impedance tomography", "Python", "Unstructural Mesh"],
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    zip_safe=False,
-    install_requires=["numpy", "scipy", "pandas", "matplotlib", "shapely"],
+    license=__license__,
 )
