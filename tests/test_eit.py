b8e545c67b71773eb65b8ef190dda2047f75c3d4
David Metz <david.metz67@gmail.com>
2022-05-09 19:43:38 +0200

    use PyEITProtocol

--- tests/test_eit.py
+++ tests/test_eit.py
@@ -2,6 +2,7 @@
 import unittest
 import numpy as np
 import pyeit.eit
+from pyeit.eit.protocol import PyEITProtocol, build_meas_pattern_std
 from pyeit.mesh.wrapper import PyEITMesh
 
 
@@ -14,13 +15,16 @@ def _mesh_obj():
 
     return PyEITMesh(node=node, element=element, perm=perm, el_pos=el_pos, ref_el=3)
 
+def _protocol_obj(ex_mat, n_el, step_meas, parser_meas):
+    meas_mat = build_meas_pattern_std(ex_mat, n_el, step_meas, parser_meas)
+    return PyEITProtocol(ex_mat, meas_mat)
 
 class TestFem(unittest.TestCase):
     def test_bp(self):
         """test back projection"""
         mesh = _mesh_obj()
         ex_mat = np.array([[0, 1], [1, 0]])
-        protocol = {"ex_mat": ex_mat, "step": 1, "parser": "meas_current"}
+        protocol= _protocol_obj(ex_mat, mesh.n_el, 1, "meas_current")
         solver = pyeit.eit.BP(mesh=mesh, protocol=protocol)
         solver.setup()
 
