97982a49989a46c25f5eb16ea9f950b71c299154
JacobTh98 <jacob.thoenes@googlemail.com>
2022-10-07 14:59:07 +0200

    modified

--- pyeit/mesh/mesh_img.py
+++ pyeit/mesh/mesh_img.py
@@ -1,39 +1,34 @@
-""" convert an image to pyeit mesh """
+""" convert an image to pyeit mesh with version <1.2.0"""
 # https://github.com/JacobTh98/pyEIT_image_wrapper
 
 # Heed requirements.txt
 import numpy as np
 import matplotlib.pyplot as plt
-import pyeit.mesh as mesh
 import math
 import cv2
-
-import numpy as np
-import matplotlib.pyplot as plt
 import pyeit.mesh as mesh
-import math
-import cv2
+from pyeit.mesh import create
 
 # Option to plot the generated mesh.
-def plot_mesh(mesh_obj, el_pos):
+def plot_mesh(mesh_obj):
     """
     Plot mesh
     
-    input:  - mesh_obj: mesh_obj (pyEIT)
-            - el_pos: electrode sequence (array)
+    input:  - mesh_obj: mesh_obj (PyEITMesh)
+    
     """
     plt.style.use('default')
-    pts = mesh_obj['node']
-    tri = mesh_obj['element']
+    pts = mesh_obj.node
+    tri = mesh_obj.element
     x, y = pts[:, 0], pts[:, 1]
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.tripcolor(x, y, tri, np.real(mesh_obj['perm']),
+    ax.tripcolor(x, y, tri, np.real(mesh_obj.perm),
                  edgecolors='k', shading='flat', alpha=0.5,
                  cmap=plt.cm.viridis)
     # draw electrodes
-    ax.plot(x[el_pos], y[el_pos], 'ro')
-    for i, e in enumerate(el_pos):
+    ax.plot(x[mesh_obj.el_pos], y[mesh_obj.el_pos], 'ro')
+    for i, e in enumerate(mesh_obj.el_pos):
         ax.text(x[e], y[e], str(i+1), size=12)
     ax.set_title(r'mesh')
     ax.set_aspect('equal')
@@ -110,30 +105,21 @@ def geometry_to_img_wrot(objct='circle',r=0.5,phi=0,d=0.5):
 #The wrapper function to transform a 200x200 picture to the pyEIT mesh.
 def groundtruth_IMG_based(IMG, n_el=16, perm_empty_gnd = 1,perm_obj = 10):
     """
-    Wraps a image to the pyEIT mesh area.
+    Wraps a image to the PyEITMesh area.
     
     input:  - 200x200 image
             - n_el: number of electrodes
             - perm_empty_gnd: perm of the empty ground
             - perm_obj: perm ob the object area
-    return: mesh_obj (pyEIT)
+    return: mesh_obj (PyEITMesh)
     """
-    def generate_mesh(n_el=n_el, perm=perm_obj):
-        def _fd(pts):
-            if pts.ndim == 1:
-                pts = pts[np.newaxis]
-            a, b = 1.0, 1.0
-            return np.sum((pts/[a, b])**2, axis=1) - 1.0
-        # Change h0 for a more filigree or rawer mesh.
-        mesh_obj, el_pos = mesh.create(n_el, fd=_fd, h0=0.05)
-        return mesh_obj
+    mesh_obj = create(n_el=n_el, h0=0.05)
     X_Y=np.array(np.where(IMG==1))
     X = X_Y[1,:] -100
     Y = (X_Y[0,:] -100)*-1
-    mesh_obj = generate_mesh()
-    pts = mesh_obj["element"]
-    tri = mesh_obj["node"]
-    perm = mesh_obj["perm"].copy()
+    pts = mesh_obj.element
+    tri = mesh_obj.node
+    perm = mesh_obj.perm.copy()
     tri_centers = np.mean(tri[pts], axis=1)
     mesh_x = np.round(tri_centers[:,0]*100)
     mesh_y = np.round(tri_centers[:,1]*100)
@@ -142,5 +128,5 @@ def groundtruth_IMG_based(IMG, n_el=16, perm_empty_gnd = 1,perm_obj = 10):
         for j in range(len(mesh_x)):
             if X[i] == mesh_x[j] and Y[i] == mesh_y[j]:
                 Perm[j] = perm_obj
-    mesh_obj['perm'] = Perm
+    mesh_obj.perm = Perm
     return mesh_obj
