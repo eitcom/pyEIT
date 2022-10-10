d0328c700327d34720865a46b24fe6e55ef6523d
JacobTh98 <jacob.thoenes@googlemail.com>
2022-10-10 15:02:29 +0200

    Added two functions "mesh_img.py" and "plot_mesh.py".
    The file "mesh_img.py" provides a way to generate any kind of object inside the pyEIT unit circle mesh.
    
    With "plot_mesh.py" it is possible to plot a pyEITmesh.

---
+++ pyeit/mesh/plot_mesh.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_mesh(mesh_obj):
+    """Plot PyEITMesh """
+    plt.style.use("default")
+    pts = mesh_obj.node
+    tri = mesh_obj.element
+    x, y = pts[:, 0], pts[:, 1]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.tripcolor(
+        x,
+        y,
+        tri,
+        np.real(mesh_obj.perm),
+        edgecolors="k",
+        shading="flat",
+        alpha=0.5,
+        cmap=plt.cm.viridis,
+    )
+    # draw electrodes
+    ax.plot(x[mesh_obj.el_pos], y[mesh_obj.el_pos], "ro")
+    for i, e in enumerate(mesh_obj.el_pos):
+        ax.text(x[e], y[e], str(i + 1), size=12)
+    ax.set_title(r"mesh")
+    ax.set_aspect("equal")
+    ax.set_ylim([-1.2, 1.2])
+    ax.set_xlim([-1.2, 1.2])
+    fig.set_size_inches(6, 6)
